97bc05b5e76f378aa2d056a2a5d27d62f95ecd1e
TheDeployGuy <jason.lloyddev@gmail.com>
2018-12-14 23:44:02 +0000

    Convert sell component to ts

--- sick-fits/frontend/pages/sell.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function sell() {
-  return (
-    <div>
-        <p>Sell!</p>
-    </div>
-  )
-}
---
+++ sick-fits/frontend/pages/sell.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sell: React.SFC<{}> = () => (
+  <div>
+  <p>Sell!</p>
+</div>
+)
+
+export default Sell;
