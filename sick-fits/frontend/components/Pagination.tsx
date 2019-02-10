51168a297a532be7828ae6e722f4f48fa8e801d3
TheDeployGuy <jason.lloyddev@gmail.com>
2019-02-10 21:18:07 +0000

    Remove duplication

--- sick-fits/frontend/components/Pagination.tsx
+++ sick-fits/frontend/components/Pagination.tsx
@@ -24,35 +24,36 @@ const Pagination: React.SFC<{
         if (loading) return <p>Loading...</p>;
         const count = data.itemsConnection.aggregate.count;
         const pages = Math.ceil(count / perPage);
+        const currentPage = props.page;
         return (
           <PaginationStyles>
             <Head>
               <title>
-                Sicks Fits - Page {props.page} of {pages}
+                Sicks Fits - Page {currentPage} of {pages}
               </title>
             </Head>
             <Link
               prefetch
               href={{
                 pathname: "items",
-                query: { page: props.page - 1 }
+                query: { page: currentPage - 1 }
               }}
             >
-              <a className="prev" aria-disabled={props.page <= 1}>
+              <a className="prev" aria-disabled={currentPage <= 1}>
                 Prev
               </a>
             </Link>
             <p>
-              Page {props.page} of {pages}
+              Page {currentPage} of {pages}
             </p>
             <Link
               prefetch
               href={{
                 pathname: "items",
-                query: { page: props.page + 1 }
+                query: { page: currentPage + 1 }
               }}
             >
-              <a className="next" aria-disabled={props.page >= pages}>
+              <a className="next" aria-disabled={currentPage >= pages}>
                 Next
               </a>
             </Link>
