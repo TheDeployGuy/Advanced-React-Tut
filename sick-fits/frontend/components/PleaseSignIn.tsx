412d1a9afba4d61980c2f05795e40260fc956dec
TheDeployGuy <jason.lloyddev@gmail.com>
2019-02-27 08:16:02 +0000

    Video #33: Create a gated sign in component

---
+++ sick-fits/frontend/components/PleaseSignIn.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { CURRENT_USER_QUERY } from "./User";
+import Signin from "./Signin";
+
+interface PleaseSignInProps {
+  children: React.ReactNode;
+}
+const PleaseSignIn: React.SFC<PleaseSignInProps> = props => (
+  <Query query={CURRENT_USER_QUERY}>
+    {({ data, loading }) => {
+      if (loading) return <p>Loading...</p>;
+      if (!data.me) {
+        return (
+          <div>
+            <p>Please Sign in before continuing</p>
+            <Signin />
+          </div>
+        );
+      }
+      return props.children;
+    }}
+  </Query>
+);
+
+export default PleaseSignIn;
